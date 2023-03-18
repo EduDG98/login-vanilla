615907ec52a83364f946225d946ab8dbba3bd49e
Eduardo Diaz <edudug98@gmail.com>
2023-03-18 14:52:04 +0000

    handleLogin with firebase

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
+import { firebaseConfig } from "./firabase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+
 const submitButton = document.querySelector("button");
-const label = document.querySelector("label + label");
-const warming = document.createElement("div");
 
-const handleLogin = () => {
-  const password = document.querySelector("#password");
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
 
-  if (password.value.length < 6) {
-    console.log("too short");
-    warming.textContent = "Password too short";
-    warming.classList.add("warming");
-    label.appendChild(warming);
-  }
+const handleLogin = (event) => {
+  event.preventDefault();
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("dentro");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("fuera");
+    });
 };
 
 submitButton.addEventListener("click", handleLogin);
